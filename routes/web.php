1e3519b596926a109271640fd4c8faa1f7c58250
alvian21 <alvian.ardhiansyah@gmail.com>
2021-05-21 17:55:28 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/", function(){
+    return view('welcome');
+});
+
 Route::get("/login", "Frontend\AuthController@getLogin");
 Route::post("/login", "Frontend\AuthController@login")->name("login");
 
