fe76fd86852fd73e7f81c01d45b7bed9f3d97eb1
alvian21 <alvian.ardhiansyah@gmail.com>
2021-05-22 11:50:27 +0700

    revisi

--- app/Imports/CsvImport.php
+++ app/Imports/CsvImport.php
@@ -19,100 +19,103 @@ class CsvImport implements ToCollection
      */
     public function collection(Collection $collection)
     {
-        // dd($collection);
+
         $month = date('m');
         $year = date('Y');
 
         foreach ($collection as $key => $value) {
             if ($key > 0) {
-                $trsaldosimpanan = new Trsaldosimpanan();
-                $trsaldosimpanan->Tanggal = date('Y-m-d H:i:s');
-                $trsaldosimpanan->KodeUser = $value[0];
-                $trsaldosimpanan->Saldo = $value[1];
-                $trsaldosimpanan->save();
+                $anggota = Msanggota::where("Kode", $value[0])->first();
+                if($anggota){
+                    $trsaldosimpanan = new Trsaldosimpanan();
+                    $trsaldosimpanan->Tanggal = date('Y-m-d H:i:s');
+                    $trsaldosimpanan->KodeUser = $value[0];
+                    $trsaldosimpanan->Saldo = $value[1];
+                    $trsaldosimpanan->save();
 
-
-                $trperiode = Trtransaksiperiode::where('KodeUser', $value[0])->whereMonth('LastUpdate', $month)->whereYear('LastUpdate', $year)->OrderBy('LastUpdate', 'DESC')->first();
-                if ($trperiode) {
-                    $formatNomor = $trperiode->Nomor;
-                } else {
-                    $trperiode = Trtransaksiperiode::whereMonth('LastUpdate', $month)->whereYear('LastUpdate', $year)->OrderBy('LastUpdate', 'DESC')->first();
+                    $trperiode = Trtransaksiperiode::where('KodeUser', $value[0])->whereMonth('LastUpdate', $month)->whereYear('LastUpdate', $year)->OrderBy('LastUpdate', 'DESC')->first();
                     if ($trperiode) {
-                        $substr = substr($trperiode->Nomor, -5);
-                        $substr = (int) str_replace('-', '', $substr);
-                        $nomor = $substr + 1;
-                        $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
-                        $formatNomor = "TB-" . date('y-m-d') . "-" . $addzero;
+                        $formatNomor = $trperiode->Nomor;
                     } else {
-                        $nomor = 1;
-                        $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
-                        $formatNomor = "TB-" . date('y-m-d') . "-" . $addzero;
+                        $trperiode = Trtransaksiperiode::whereMonth('LastUpdate', $month)->whereYear('LastUpdate', $year)->OrderBy('LastUpdate', 'DESC')->first();
+                        if ($trperiode) {
+                            $substr = substr($trperiode->Nomor, -5);
+                            $substr = (int) str_replace('-', '', $substr);
+                            $nomor = $substr + 1;
+                            $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
+                            $formatNomor = "TB-" . date('y-m-d') . "-" . $addzero;
+                        } else {
+                            $nomor = 1;
+                            $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
+                            $formatNomor = "TB-" . date('y-m-d') . "-" . $addzero;
+                        }
+                    }
+
+
+                    if ($value[2] != null) {
+                        $trsaldohutang = new Trsaldohutang();
+                        $trsaldohutang->Tanggal = date('Y-m-d H:i:s');
+                        $trsaldohutang->KodeUser = $value[0];
+                        $trsaldohutang->Saldo =  $value[2];
+                        $trsaldohutang->BayarBerapaKali =  $value[3];
+                        $trsaldohutang->CicilanTotal =  $value[4];
+                        $trsaldohutang->TotalBerapaKali =  $value[5];
+                        $trsaldohutang->save();
+
+                        $trtransaksiperiode = new Trtransaksiperiode();
+                        $trtransaksiperiode->Nomor = $formatNomor;
+                        $trtransaksiperiode->Periode = date('Ym');
+                        $trtransaksiperiode->KodeUser = $value[0];
+                        $trtransaksiperiode->KodeTransaksi = "20";
+                        $trtransaksiperiode->Nilai = $value[2];
+                        $trtransaksiperiode->UserUpdate = Auth::guard('web')->user()->UserLogin;
+                        $trtransaksiperiode->LastUpdate =  date('Y-m-d H:i:s');
+                        $trtransaksiperiode->save();
+
+
+                        $trpinjaman = Trpinjaman::whereMonth('LastUpdate', $month)->whereYear('LastUpdate', $year)->OrderBy('LastUpdate', 'DESC')->first();
+                        if ($trpinjaman) {
+                            $substr = substr($trpinjaman->Nomor, -5);
+                            $substr = (int) str_replace('-', '', $substr);
+                            $nomor = $substr + 1;
+                            $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
+                            $formatNomorpinjaman = "PI-" . date('y-m-d') . "-" . $addzero;
+                        } else {
+                            $nomor = 1;
+                            $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
+                            $formatNomorpinjaman = "PI-" . date('y-m-d') . "-" . $addzero;
+                        }
+
+                        $anggota = Msanggota::where("Kode", $value[0])->first();
+                        $mscicilan = Mscicilan::where("Bulan", $value[5])->where("CicilanTotal", $value[4])->first();
+                        $datatrpinjaman = new Trpinjaman();
+                        $datatrpinjaman->Nomor = $formatNomorpinjaman;
+                        $datatrpinjaman->KodeAnggota = $value[0];
+                        $datatrpinjaman->SubDept = $anggota->SubDept;
+                        $datatrpinjaman->Pinjaman = $mscicilan->Nominal;
+                        $datatrpinjaman->CicilanTotal = $mscicilan->CicilanTotal;
+                        $datatrpinjaman->BerapaKaliBayar = $value[5];
+                        $datatrpinjaman->CicilanPokok = $mscicilan->CicilanPokok;
+                        $datatrpinjaman->CicilanBunga = $mscicilan->CicilanBunga;
+                        $datatrpinjaman->Alasan = "Saldo Awal";
+                        $datatrpinjaman->TanggalPengajuan = date('Y-m-d');
+                        $datatrpinjaman->UserUpdate = Auth::guard('web')->user()->UserLogin;
+                        $datatrpinjaman->ApprovalStatus = "DISETUJUI";
+                        $datatrpinjaman->PengajuanPinjaman = $mscicilan->Nominal;
+                        $datatrpinjaman->save();
+                    } else {
+                        $trtransaksiperiode = new Trtransaksiperiode();
+                        $trtransaksiperiode->Nomor = $formatNomor;
+                        $trtransaksiperiode->Periode = date('Ym');
+                        $trtransaksiperiode->KodeUser = $value[0];
+                        $trtransaksiperiode->KodeTransaksi = "01";
+                        $trtransaksiperiode->Nilai = $value[1];
+                        $trtransaksiperiode->UserUpdate = Auth::guard('web')->user()->UserLogin;
+                        $trtransaksiperiode->LastUpdate =  date('Y-m-d H:i:s');
+                        $trtransaksiperiode->save();
                     }
                 }
 
-
-                if ($value[2] != null) {
-                    $trsaldohutang = new Trsaldohutang();
-                    $trsaldohutang->Tanggal = date('Y-m-d H:i:s');
-                    $trsaldohutang->KodeUser = $value[0];
-                    $trsaldohutang->Saldo =  $value[2];
-                    $trsaldohutang->BayarBerapaKali =  $value[3];
-                    $trsaldohutang->CicilanTotal =  $value[4];
-                    $trsaldohutang->TotalBerapaKali =  $value[5];
-                    $trsaldohutang->save();
-
-                    $trtransaksiperiode = new Trtransaksiperiode();
-                    $trtransaksiperiode->Nomor = $formatNomor;
-                    $trtransaksiperiode->Periode = date('Ym');
-                    $trtransaksiperiode->KodeUser = $value[0];
-                    $trtransaksiperiode->KodeTransaksi = "20";
-                    $trtransaksiperiode->Nilai = $value[2];
-                    $trtransaksiperiode->UserUpdate = Auth::guard('web')->user()->UserLogin;
-                    $trtransaksiperiode->LastUpdate =  date('Y-m-d H:i:s');
-                    $trtransaksiperiode->save();
-
-
-                    $trpinjaman = Trpinjaman::whereMonth('LastUpdate', $month)->whereYear('LastUpdate', $year)->OrderBy('LastUpdate', 'DESC')->first();
-                    if ($trpinjaman) {
-                        $substr = substr($trpinjaman->Nomor, -5);
-                        $substr = (int) str_replace('-', '', $substr);
-                        $nomor = $substr + 1;
-                        $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
-                        $formatNomorpinjaman = "PI-" . date('y-m-d') . "-" . $addzero;
-                    } else {
-                        $nomor = 1;
-                        $addzero =  str_pad($nomor, 4, '0', STR_PAD_LEFT);
-                        $formatNomorpinjaman = "PI-" . date('y-m-d') . "-" . $addzero;
-                    }
-
-                    $anggota = Msanggota::where("Kode", $value[0])->first();
-                    $mscicilan = Mscicilan::where("Bulan", $value[5])->where("CicilanTotal", $value[4])->first();
-                    $datatrpinjaman = new Trpinjaman();
-                    $datatrpinjaman->Nomor = $formatNomorpinjaman;
-                    $datatrpinjaman->KodeAnggota = $value[0];
-                    $datatrpinjaman->SubDept = $anggota->SubDept;
-                    $datatrpinjaman->Pinjaman = $mscicilan->Nominal;
-                    $datatrpinjaman->CicilanTotal = $mscicilan->CicilanTotal;
-                    $datatrpinjaman->BerapaKaliBayar = $value[5];
-                    $datatrpinjaman->CicilanPokok = $mscicilan->CicilanPokok;
-                    $datatrpinjaman->CicilanBunga = $mscicilan->CicilanBunga;
-                    $datatrpinjaman->Alasan = "Saldo Awal";
-                    $datatrpinjaman->TanggalPengajuan = date('Y-m-d');
-                    $datatrpinjaman->UserUpdate = Auth::guard('web')->user()->UserLogin;
-                    $datatrpinjaman->ApprovalStatus = "DISETUJUI";
-                    $datatrpinjaman->PengajuanPinjaman = $mscicilan->Nominal;
-                    $datatrpinjaman->save();
-                } else {
-                    $trtransaksiperiode = new Trtransaksiperiode();
-                    $trtransaksiperiode->Nomor = $formatNomor;
-                    $trtransaksiperiode->Periode = date('Ym');
-                    $trtransaksiperiode->KodeUser = $value[0];
-                    $trtransaksiperiode->KodeTransaksi = "01";
-                    $trtransaksiperiode->Nilai = $value[1];
-                    $trtransaksiperiode->UserUpdate = Auth::guard('web')->user()->UserLogin;
-                    $trtransaksiperiode->LastUpdate =  date('Y-m-d H:i:s');
-                    $trtransaksiperiode->save();
-                }
             }
         }
 
